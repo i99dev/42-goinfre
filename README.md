821779403f24ab3eda3bed5d6d9a554d522c871f
i99dev <obaid.altuniji@gmail.com>
2022-07-12 16:22:36 +0400

    its Done

---
+++ README.md
@@ -0,0 +1,27 @@
+# 42 lab Space 
+🚩any password or extension for vscode and chrome will be remove if you run this first time.
+
+this script to fix issue with 42 lab imac space , every time i go to 42 lab to work I get error message like this:
+
+```
+You don't have space 
+```
+## Ok , What script do you think ?
+
+actually , Create link to file in ~/goinfre for:
+- Downloads folder.
+- Caches folder.
+
+and download chrome and vscode and also add to ~/goinfre and create shortcut to desktop for chrome and vscode 
+
+Now I use only 40% from 5 gb of space , so I think this script is good to use.
+
+## How use 
+
+```
+chmod +x be_free.sh
+```
+```
+$ ./be_free.sh
+```
+and just breath 😁 every thing done.
--- be_free.sh
+++ be_free.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
-# remove Caches foloder from /Library/Caches
-rm -rf ~/Library/Caches
-# create Caches folder in /goinfre/Caches
-mkdir -p ~/goinfre/Caches
-# create ln to Caches folder in /Library/Caches
-ln -s ~/goinfre/Caches ~/Library/Caches
 
-# download latest version of vscode
-curl -L https://code.visualstudio.com/sha/download\?build\=stable\&os\=darwin-universal -o /tmp/vscode.zip && unzip /tmp/vscode.zip -d ~/goinfre/
-# wait for download to finish
-# while [ ! -f /tmp/vscode.zip ]
-# do
-#     sleep 1
-# done
+green="\e[32m"
+red="\e[31m"
+reset="\e[0m"
 
-# makealias for vscode
-ln -s ~/goinfre/Visual\ Studio\ Code.app /Desktop/Visual\ Studio\ Code.app
+# ask if user at 42 lab if yes continue, if no exit
+echo -e "${green}Do you want to be free?${reset}"
+read -p "y/n: " answer
+if [ "$answer" == "y" ]; then
+    rm -rf ~/Library/Caches
+    mkdir -p ~/goinfre/Caches
+    ln -s ~/goinfre/Caches ~/Library/Caches
+    curl -L https://code.visualstudio.com/sha/download\?build\=stable\&os\=darwin-universal -o /tmp/vscode.zip && unzip /tmp/vscode.zip -d ~/goinfre/
+    ln -s ~/goinfre/Visual\ Studio\ Code.app /Desktop/Visual\ Studio\ Code.app
+    curl -L https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg -o /tmp/googlechrome.dmg
+    hdiutil attach /tmp/googlechrome.dmg
+    cp -R /Volumes/Google\ Chrome/Google\ Chrome.app ~/goinfre/
+    hdiutil detach /Volumes/Google\ Chrome
+    ln -s ~/goinfre/Google\ Chrome.app ~/Desktop/Google\ Chrome.app
+    rm -rf ~/Downloads
+    mkdir -p ~/goinfre/Downloads
+    ln -s ~/goinfre/Downloads ~/Downloads
 
-# download latest version of Chrome
-curl -L https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg -o /tmp/googlechrome.dmg
-# mount Chrome dmg
-hdiutil attach /tmp/googlechrome.dmg
-# copy Chrome to /goinfre/
-cp -R /Volumes/Google\ Chrome/Google\ Chrome.app ~/goinfre/
-# unmount Chrome dmg
-hdiutil detach /Volumes/Google\ Chrome
-# makealias for Chrome to desktop
-ln -s ~/goinfre/Google\ Chrome.app ~/Desktop/Google\ Chrome.app
+    df -h
 
-# rm Downloads 
-rm -rf ~/Downloads
-# create Downloads folder in /goinfre/Downloads
-mkdir -p ~/goinfre/Downloads
-# create ln to Downloads folder in ~/Downloads
-ln -s ~/goinfre/Downloads ~/Downloads
-
-# check space 
-df -h
-
-# echo is done with green color
-echo -e "\e[32mDone!\e[0m" # -e is used to escape the special characters
+    echo -e "${green}Done!⚠️🚧${reset}"
+else
+    echo -e "${red}Only for 42 lab!${reset}"
+    exit 1
+fi
